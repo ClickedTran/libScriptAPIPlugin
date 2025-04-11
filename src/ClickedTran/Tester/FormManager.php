a8a80d636c8863e5db19d39eeb0592d886018802
ClickedTran_VN <58399779+ClickedTran@users.noreply.github.com>
2025-04-11 17:07:00 +0700

    Update FormManager.php

--- src/ClickedTran/Tester/FormManager.php
+++ src/ClickedTran/Tester/FormManager.php
@@ -5,16 +5,18 @@ use jojoe77777\FormAPI\SimpleForm;
 
 /** @var Player $player */
 
-$form = new SimpleForm(function(Player $player, ?int $data) {
-    if ($data === null) return;
-    sayHello($player, $data);
-});
+function sendForm(Player $player) : void{
+  $form = new SimpleForm(function(Player $player, ?int $data) {
+      if ($data === null) return;
+      sayHello($player, $data);
+  });
 
-$form.setTitle("Form test");
-$form.addButton("Nút 1");
-$form.addButton("Nút 2");
+  $form.setTitle("Form test");
+  $form.addButton("Nút 1");
+  $form.addButton("Nút 2");
 
-$player.sendForm($form);
+  $player.sendForm($form);
+}
 
 function sayHello(Player $player, int $data){
     return $player.sendMessage("Bạn đã ấn vào nút số ".$data);
